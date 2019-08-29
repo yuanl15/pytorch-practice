5d2fd4f079874255d1c4372b238bfcd42be1a30c
yuanl1 <yuanl15@foxmail.com>
2019-08-29 19:50:03 +0800

    logistic data

--- NeuralNetwork/logistic_regression.py
+++ NeuralNetwork/logistic_regression.py
@@ -1,3 +1,9 @@
 import torch
 
 
+with open('logistic-data.txt') as f:
+    data_list = f.readlines()
+    data_list = [i.split('\n')[0] for i in data_list]
+    data_list = [i.split(',') for i in data_list]
+    data = [(float(i[0]), float(i[1]), float(i[2])) for i in data_list]
+    
